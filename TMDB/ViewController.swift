c1cd9133e50ea7e109acc2fe91c5cc3f7ed7cefb
Huseyin Akcay <huseyin.akcay@loodos.com>
2023-02-01 17:07:36 +0300

    Initial Commit

---
+++ TMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by Huseyin Akcay on 1.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
