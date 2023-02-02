7eef40728a6b44b646a567499d39b645c660c754
Huseyin Akcay <huseyin.akcay@loodos.com>
2023-02-03 02:40:15 +0300

    UI changes on HomeVC

--- TMDB/Screens/Home/Cell/HomeVcCollectionViewCell.swift
+++ TMDB/Screens/Home/Cell/HomeVcCollectionViewCell.swift
@@ -14,8 +14,8 @@ final class HomeVcCollectionViewCell: BaseCollectionViewCell {
         let view = UIView()
         view.layer.shadowColor = UIColor.lightGray.cgColor
         view.layer.shadowOffset = .zero
-        view.layer.shadowOpacity = 0.4
-        view.layer.shadowRadius = 7
+        view.layer.shadowOpacity = 0.6
+        view.layer.shadowRadius = 8
         view.layer.cornerRadius = 16
         view.backgroundColor = .black
         return view
@@ -43,6 +43,24 @@ final class HomeVcCollectionViewCell: BaseCollectionViewCell {
         return view
     }()
 
+    lazy private var averageVoteView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red.withAlphaComponent(0.7)
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 16
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        return view
+    }()
+
+    lazy private var averageVoteLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: commonFont, size: 12)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+
     lazy private var titleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont(name: commonFont, size: 16)
@@ -53,14 +71,35 @@ final class HomeVcCollectionViewCell: BaseCollectionViewCell {
         return label
     }()
 
+    lazy private var bottomTitleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red.withAlphaComponent(0.7)
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        return view
+    }()
+
+    lazy private var bottomTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: commonFont, size: 12)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        return label
+    }()
+
     //MARK: - Configure UI
     override func setupViews() {
         super.setupViews()
 
         contentView.addSubview(movieContainerView)
         movieContainerView.addSubview(movieStackView)
-        movieImageViewContainerView.addSubview(movieImageView)
         movieImageViewContainerView.addSubview(titleLabel)
+        movieImageViewContainerView.addSubview(movieImageView)
+        movieImageViewContainerView.addSubview(averageVoteView)
+        averageVoteView.addSubview(averageVoteLabel)
+        movieImageViewContainerView.addSubview(bottomTitleView)
+        bottomTitleView.addSubview(bottomTitleLabel)
     }
 
     override func setupLayout() {
@@ -78,12 +117,34 @@ final class HomeVcCollectionViewCell: BaseCollectionViewCell {
         titleLabel.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        averageVoteView.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview().inset(10)
+            make.size.equalTo(32)
+        }
+        averageVoteLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        bottomTitleView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(30)
+        }
+        bottomTitleLabel.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(6)
+        }
     }
 
     //MARK: - Methods
     func setCell(with model: Results?) {
-        guard let model = model,
-              let posterPath = model.posterPath else {
+        guard let model = model else { return }
+        averageVoteLabel.text = "\(model.voteAverage ?? 0.0)"
+        bottomTitleLabel.text = model.name
+
+        //MARK: - Fetch Images
+        guard let posterPath = model.posterPath else {
+            self.titleLabel.isHidden = false
+            self.titleLabel.text = model.name
+            self.bottomTitleView.isHidden = true
             return
         }
         let url = APIConstants.imageBaseUrl + posterPath
@@ -98,9 +159,11 @@ final class HomeVcCollectionViewCell: BaseCollectionViewCell {
                 switch result {
                 case .success(_):
                     self.titleLabel.isHidden = true
+                    self.bottomTitleView.isHidden = false
                 case .failure(_):
                     self.titleLabel.isHidden = false
                     self.titleLabel.text = model.name
+                    self.bottomTitleView.isHidden = true
                 }
             }
         }
